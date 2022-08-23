2d4cd175edfae60ddf70982b34dbaabcec92273b
jkalnins81 <j_kalnins@hotmail.com>
2022-08-23 13:33:28 +0200

    Text bubble can close + centered

--- Bjork&Frihet/Assets/Scripts/TextBubble.cs
+++ Bjork&Frihet/Assets/Scripts/TextBubble.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
-public class NewBehaviourScript : MonoBehaviour
+public class TextBubble : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI m_Object;
 
@@ -12,7 +12,7 @@ public class NewBehaviourScript : MonoBehaviour
         m_Object.text = "Text Bubble";
     }
 
-    void CloseTextBubble()
+    public void CloseTextBubble()
     {
         m_Object.text = "";
     }
