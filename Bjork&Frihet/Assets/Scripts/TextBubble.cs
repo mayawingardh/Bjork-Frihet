ec93c157dc12d71e3b5b00ef00741a0fdecfe0ab
jkalnins81 <j_kalnins@hotmail.com>
2022-08-24 13:25:15 +0200

    Updates

--- Bjork&Frihet/Assets/Scripts/TextBubble.cs
+++ Bjork&Frihet/Assets/Scripts/TextBubble.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 using TMPro;
 
 public class TextBubble : MonoBehaviour
@@ -9,10 +10,33 @@ public class TextBubble : MonoBehaviour
     [SerializeField] GameObject textBubbleButton;
     [SerializeField] GameObject closeButton;
 
+    public DragAndDrop dragAndDrop2;
+
+    //public GameObject gameManager;
+    //SpawnManager spawnManager;
+
+    private void OnEnable()
+    {
+        //dragAndDrop2.IsPlaced += ShowTextBubble();
+    }
+    private void OnDisable()
+    {
+        //dragAndDrop2.IsPlaced -= ShowTextBubble();
+    }
+
 
     void Start()
     {
+        //spawnManager = gameManager.GetComponent<SpawnManager>();
         m_Object.text = "Text Bubble";
+    }
+
+    
+
+    void ShowTextBubble()
+    {
+        textBubbleButton.SetActive(true);
+        closeButton.SetActive(true);
     }
 
     public void CloseTextBubble()
